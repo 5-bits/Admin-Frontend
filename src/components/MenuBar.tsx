7dabdfb31ebcaf9f3c71ef1fe7ab10e6a7529d39
PratikGarai <pratikgarai0208@gmail.com>
2021-02-15 22:04:32 +0530

    divided the drawer into 2 parts

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -8,13 +7,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+
+import DrawerComponents from './DrawerComponents';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -35,11 +29,10 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const MenuBar = () => {
 
-    type Anchor = 'top' | 'left' | 'bottom' | 'right';
     const classes = useStyles();
     const [drawer, setDrawer] = React.useState(false);
 
-    const toggleDrawer = (anchor: Anchor, open: boolean) => (
+    const toggleDrawer = () => (
         event: React.KeyboardEvent | React.MouseEvent,
         ) => {
         if (
@@ -53,41 +46,14 @@ const MenuBar = () => {
         setDrawer(!drawer);
     };
 
-    const list = (anchor: Anchor) => (
-        <div
-          className={classes.list}
-          role="presentation"
-          onClick={toggleDrawer(anchor, false)}
-          onKeyDown={toggleDrawer(anchor, false)}
-        >
-          <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      );
-
     return(
         <div className={classes.root}>
         <AppBar position="static">
-            <Drawer anchor="left" open={drawer} onClose={toggleDrawer("left", false)}>
-                {list("left")}
+            <Drawer anchor="left" open={drawer} onClose={toggleDrawer()}>
+                <DrawerComponents toggleDrawer={toggleDrawer} />
             </Drawer>
             <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer("left", true)}>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer()}>
                 <MenuIcon />
             </IconButton>
             <Typography variant="h6" className={classes.title}>
