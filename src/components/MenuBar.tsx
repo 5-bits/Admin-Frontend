cc238b6121e4e8170d3ed0c08f0ad7b89d5a02fb
PratikGarai <pratikgarai0208@gmail.com>
2021-02-15 21:54:37 +0530

    added sample drawer

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,24 +27,73 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       flexGrow: 1,
     },
+    list: {
+        width: 250,
+    },
   }),
 );
 
 const MenuBar = () => {
 
+    type Anchor = 'top' | 'left' | 'bottom' | 'right';
     const classes = useStyles();
+    const [drawer, setDrawer] = React.useState(false);
+
+    const toggleDrawer = (anchor: Anchor, open: boolean) => (
+        event: React.KeyboardEvent | React.MouseEvent,
+        ) => {
+        if (
+            event.type === 'keydown' &&
+            ((event as React.KeyboardEvent).key === 'Tab' ||
+            (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+            return;
+        }
+
+        setDrawer(!drawer);
+    };
+
+    const list = (anchor: Anchor) => (
+        <div
+          className={classes.list}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      );
 
     return(
         <div className={classes.root}>
         <AppBar position="static">
+            <Drawer anchor="left" open={drawer} onClose={toggleDrawer("left", false)}>
+                {list("left")}
+            </Drawer>
             <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer("left", true)}>
                 <MenuIcon />
             </IconButton>
             <Typography variant="h6" className={classes.title}>
                 Medibot Admin
             </Typography>
-            <Button color="inherit">Dummy</Button>
+            <Button color="inherit">Add Client</Button>
             </Toolbar>
         </AppBar>
         </div>
