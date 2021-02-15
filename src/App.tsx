4c7517e6e2c60f19068ba69f50c8f3dba68cf928
PratikGarai <pratikgarai0208@gmail.com>
2021-02-15 21:35:12 +0530

    menubar made

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+
+import MenuBar from "./components/MenuBar";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <MenuBar />
+      <h1>Hello World</h1>
     </div>
   );
 }
